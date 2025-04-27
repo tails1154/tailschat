64f8a8f42ba4a72977c3d7bcf5ab1c66cb45b0f8
Timothy Heironimus <timothyheironimus6+github@gmail.com>
2025-04-26 20:11:49 -0500

    syntax error fixes

--- server.js
+++ server.js
@@ -1,41 +1,42 @@
 const express = require('express');
-
 const app = express();
 
+const messages = [];
+const joinedusers = [];
 
-messages = [];
-
-joinedusers = [];
-
-app.get('/api/join', (req, res) {
+app.get('/api/join', (req, res) => {
     const username = req.query.username;
 
-
-    joinedusers.push(username)
-
-    messages.push(`${username} has joined`);
-
-    res.status(200).send("JOINOK");
+    if (username) {
+        joinedusers.push(username);
+        messages.push(`${username} has joined`);
+        res.status(200).send("JOINOK");
+    } else {
+        res.status(400).send("JOINFAILNOUSERNAME");
+    }
 });
 
-
-app.get('/api/sendmsg', (req, res) {
+app.get('/api/sendmsg', (req, res) => {
     const username = req.query.username;
     const message = req.query.message;
 
     if (joinedusers.includes(username)) {
-        messages.push(username + ": " + message);
-    }
-    else {
+        messages.push(`${username}: ${message}`);
+        res.status(200).send("SENDOK");
+    } else {
         res.status(403).send("SENDFAILNOTEXIST");
     }
 });
 
-
-app.get('/api/msglist', (req, res) {
+app.get('/api/msglist', (req, res) => {
     res.status(200).json(messages);
 });
 
-app.get('/api/users', (req, res) {
+app.get('/api/users', (req, res) => {
     res.status(200).json(joinedusers);
+});
+
+// Optional: Start the server
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
 });
